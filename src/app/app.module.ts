65acfa17d47b1c57fe51d786566e27adcce89165
Juan Alberto Raya <jjrr1307@gmail.com>
2021-02-15 13:48:43 +0100

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { PrimerComponenteComponent } from './primer-componente/primer-componente
     PrimerComponenteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
